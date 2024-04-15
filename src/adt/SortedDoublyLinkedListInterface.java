e2252815a572b6cbbaf2647d91a72fb88253e044
Adam Quah <57474587+adamquah@users.noreply.github.com>
2024-04-15 15:40:14 +0800

    Add files via upload
    Use this adt for the list (confirmed correct)

---
+++ src/adt/SortedDoublyLinkedListInterface.java
@@ -0,0 +1,22 @@
+package adt;
+
+public interface SortedDoublyLinkedListInterface<T> {
+
+    public boolean add(T newEntry);
+
+    public T remove(int givenPosition);
+
+    public void clear();
+
+    public boolean replace(int givenPosition, T newEntry);
+
+    public T getEntry(int givenPosition);
+
+    public boolean contains(T anEntry);
+
+    public int getNumberOfEntries();
+
+    public boolean isEmpty();
+
+    public boolean isFull();
+}
