f711cf8d03e7881582e8fbf4ae4be9f6545fa8ec
Adam Quah <57474587+adamquah@users.noreply.github.com>
2024-04-20 00:09:12 +0800

    Update CourseAssign.java

--- src/control/CourseAssign.java
+++ src/control/CourseAssign.java
@@ -1,35 +1,30 @@
 package control;
 
-//comment
-import adt.*;
+import adt.ListInterface;
+import adt.ArrayList;
 import Boundary.Course_Assign_MaintenanceUI;
 import Entity.*;
 
+
+
 public class CourseAssign {
 
-    /**
-     * @param args the command line arguments
-     */+
-     
-       private ArrayList<TutorAssign> tutorAssign = new ArrayList<>();
-       //private ProductDAO productDAO = new ProductDAO();
-       private Course_Assign_MaintenanceUI maintenanceUI = new Course_Assign_MaintenanceUI();
-       
-       
-       
-       public tutorMaintenance(){
-           
-           
-       }
-       
- 
-        // TODO code application logic here
-        
-        public void AddTutorName(){
-            
-        }
-        
-        public void RemoveTutorName(){
-    }
-    
+    private ListInterface<TutorAssign> tutorList = new ArrayList<>();
+    private Course tutorDAO = new TutorDAO();
+    private Course_Assign_MaintenanceUI maintenanceUI = new Course_Assign_MaintenanceUI();
 
+    // Constructor
+    public CourseAssign() {
+        tutorList = tutorDAO.retrieveFromFile();
+    }
+
+    // Add a tutor's name to the list
+    public void addTutorName() {
+        // Add implementation here
+    }
+
+    // Remove a tutor's name from the list
+    public void removeTutorName() {
+        // Add implementation here
+    }
+}
