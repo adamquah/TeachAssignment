79ed34731259f22ff6756187a0a6809d57e62809
Adam Quah <57474587+adamquah@users.noreply.github.com>
2024-04-20 14:10:52 +0800

    Update CourseAssign.java

--- src/control/CourseAssign.java
+++ src/control/CourseAssign.java
@@ -1,30 +1,59 @@
 package control;
 
-import adt.ListInterface;
-import adt.ArrayList;
+//comment
+import adt.*;
 import Boundary.Course_Assign_MaintenanceUI;
 import Entity.*;
+import dao.*;
 
-
+import java.util.LinkedList;
+import java.util.List;
 
 public class CourseAssign {
 
-    private ListInterface<TutorAssign> tutorList = new ArrayList<>();
-    private Course tutorDAO = new TutorDAO();
-    private Course_Assign_MaintenanceUI maintenanceUI = new Course_Assign_MaintenanceUI();
-
-    // Constructor
-    public CourseAssign() {
-        tutorList = tutorDAO.retrieveFromFile();
+     
+    private ListInterface<TutorAssign> tutorAssignList = new ArrayList<>();
+    private ListInterface<TutorCourses> courses = new ArrayList<>();
+    private TutorDAO tutorDAO = new TutorDAO();
+    private CourseInitializer courseInitializer = new CourseInitializer();
+       //private Course_Assign_MaintenanceUI maintenanceUI = new Course_Assign_MaintenanceUI();
+       
+    public  CourseAssign() {
+        tutorAssignList = tutorDAO.retrieveFromFile();
+        courses = courseInitializer.initializeCourses();
     }
 
-    // Add a tutor's name to the list
-    public void addTutorName() {
-        // Add implementation here
+    public List<TutorAssign> getTutorAssignListAsList() {
+
+        // Create a new list instance.
+        List<TutorAssign> list = new LinkedList<TutorAssign>();
+        // Iterate over elements of ListInterface and add them to the List.
+        for(int i = 1; i <= tutorAssignList.getNumberOfEntries(); i++) {
+            list.add(tutorAssignList.getEntry(i));
+        }
+        return list;
     }
 
-    // Remove a tutor's name from the list
-    public void removeTutorName() {
-        // Add implementation here
+    public void addNewTutorAssign(TutorAssign newTutorAssign) {
+        tutorAssignList.add(newTutorAssign);
+        tutorDAO.saveToFile(tutorAssignList);
+  }
+
+    public void updateTutorAssign(int index,TutorAssign editTutorAssign) {
+        tutorAssignList.replace(index, editTutorAssign);
+        tutorDAO.saveToFile(tutorAssignList);
     }
+
+    public List<TutorCourses> coursesArrayList() {
+        // Create a new list instance.
+        List<TutorCourses> list = new LinkedList<TutorCourses>();
+        // Iterate over elements of ListInterface and add them to the List.
+        for(int i = 1; i <= courses.getNumberOfEntries(); i++) {
+            list.add(courses.getEntry(i));
+        }
+        return list;
+    }
+    
+
 }
+
